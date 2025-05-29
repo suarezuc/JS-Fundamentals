e407ea88c72a552e38d149e1dc86cfc689257b5b
suarezuc <suarezuc@gmail.com>
2025-05-29 10:40:59 -0700

    Add 7-multi_c.js: prints 'C is fun' x times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,9 @@
-const count = parseInt(process.argv[2]);
+const x = parseInt(process.argv[2]);
 
-if (isNaN(count)) {
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < count; i++) {
-    console.log('C is fun');
-  }
+  for (let i = 0; i < x; i++) console.log('C is fun');
 }
+
 
